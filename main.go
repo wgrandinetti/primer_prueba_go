0b2b869887d64eacbab1902c125cad1e66b6069e
wgrandinetti <wgrandinetti@gmail.com>
2021-08-31 20:16:11 -0300

    Agrega formulario actor

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
 	http.HandleFunc("/", Inicio)
+	http.HandleFunc("/actor", Actor)
+
 	log.Println("Servidor corriendo.")
 	http.ListenAndServe(":8081", nil)
 }
@@ -17,4 +19,9 @@ func main() {
 func Inicio(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Hola mundo.")
 	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+func Actor(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hola mundo.")
+	plantillas.ExecuteTemplate(w, "crear", nil)
 }
